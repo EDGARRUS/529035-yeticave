39874607cb689b7272ff48d35eaee6ed8b051ae4
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2019-02-26 23:01:42 +0300

    Update lot.php

--- lot.php
+++ lot.php
@@ -9,7 +9,9 @@ if ($result) {
     $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-$id = mysqli_real_escape_string($link, $_GET['id']);
+if (isset($_GET['id'])) {
+    $id = mysqli_real_escape_string($link, $_GET['id']);
+}
 
 $sql = "SELECT TIMEDIFF(lots.date_end, NOW()) as live, bets.amount + lots.step_price as future_price, lots.id, lots.date_create, lots.name, lots.start_price, lots.description, lots.step_price, lots.image, bets.amount as now_price, categories.name as cat FROM lots LEFT JOIN categories ON lots.category_id = categories.id LEFT JOIN bets ON lots.id = bets.lot_id WHERE lots.id = '%s' ORDER BY bets.amount DESC limit 1";
 
